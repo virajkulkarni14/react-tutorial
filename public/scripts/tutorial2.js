b9b644c047b32eb0bc0b1b205761d4d5d606f588
Viraj Kulkarni <viraj.kulkarni@sjsu.edu>
2015-06-25 02:56:07 -0700

    Adding tut2 skeleton

---
+++ public/scripts/tutorial2.js
@@ -0,0 +1,19 @@
+var CommentList = React.createClass({
+  render: function()  {
+    return (
+      <div className="commentList">
+        Hello, Earthlings! I'm a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function()  {
+    return (
+      <div className="commentForm">
+        Hello there! I'm a CommentForm.
+      </div>
+    );
+  }
+});
